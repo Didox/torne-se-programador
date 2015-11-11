ccbdff1dd41e79fde5e3582477032775466ebf93
Didox <dansantos@abril.com.br>
2015-11-10 23:07:19 -0200

    orientação a objetos aula 1

---
+++ 92 - Orientacao a objetos/OO_pessoa/pessoa.js
@@ -0,0 +1,35 @@
+var Pessoa = function(){
+  // atributos publicos
+  this.cpf;
+  this.nome;
+  this.endereco;
+
+  // atributos privados
+  var mensagem = "este atributo mensagem é privado, ou seja dentro da minha classe"
+
+  // metodos privados
+  var concatenaNomeEndereco = function(nome, endereco){
+    return "Nome: " + nome + ", Endereço: " + endereco;
+  }
+
+  var formataCPF = function(cpfs){
+    return  cpfs.substring(0,2) + " - " + cpfs;
+  }
+
+  // metodos publicos
+  this.informacoes = function(){
+    alert("Nome: " + this.nome + "\nCPF: " + this.cpf + "\nEndereço: " + this.endereco)
+  }
+
+  this.mostrarMensagemPrivada = function(){
+    return mensagem;
+  }
+
+  this.nomeEndereco = function(){
+    return concatenaNomeEndereco(this.nome, this.endereco);
+  }
+
+  this.cpfFormatado = function(){
+    return formataCPF(this.cpf);
+  }
+}
