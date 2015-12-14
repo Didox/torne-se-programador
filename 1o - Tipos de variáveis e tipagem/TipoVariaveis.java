81b0f8ae25ee3e17b6d610019e47cb11c84a140c
Didox <dansantos@abril.com.br>
2015-12-14 08:30:59 -0200

    tipos, tipagem e alocações

---
+++ 1o - Tipos de variáveis e tipagem/TipoVariaveis.java
@@ -0,0 +1,34 @@
+class TipoVariaveis{
+  public static void main(String[]args){
+
+    String s = "ssss";
+
+	char c = 'c';
+
+	byte by = 0;
+
+	int i = 1;
+
+	long l = 999999999;
+
+	float f = 1.99f;
+
+	double d = 999999999.5;
+
+	boolean b = true;
+
+
+	/*
+	// UTILIZADO EM C#
+
+	// TIPAGEM OU ALOCAÇÃO DINAMIMA
+	var s = "ssss";
+	var i = 1;
+
+	// TIPAGEM OU ALOCAÇÃO STATICA
+	string s = "ssss";
+	int i = 1;
+	
+	*/
+  }
+}
---
+++ 1o - Tipos de variáveis e tipagem/TipoVariaveis.rb
@@ -0,0 +1,15 @@
+s = "ssss"
+puts s.class
+
+i = 1
+puts i.class
+
+l = 99999999999999999999999
+puts l.class
+
+f = 1.5
+puts f.class
+
+b = true
+puts b.class
+
