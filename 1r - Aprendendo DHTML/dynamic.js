f7dd09ad7024dbe6851b3e29e11ec88ddce18e1d
Didox <dansantos@abril.com.br>
2016-02-02 07:07:17 -0200

    terminando aula DHTML

--- 1r - Aprendendo DHTML/dynamic.js
+++ 1r - Aprendendo DHTML/dynamic.js
@@ -5,7 +5,37 @@ function cadastrarPessoas(){
 	var telefone = document.getElementById("telefone").value;
 	var endereco = document.getElementById("endereco").value;
 
-	var html = "<tr>";
+	// primeiro tipo de validação
+	if(nome == ""){
+		alert("Por favor digite seu nome")
+		document.getElementById("nome").focus();
+		return;
+	}
+
+	if(cpf == ""){
+		alert("Por favor digite seu cpf")
+		document.getElementById("cpf").focus();
+		return;
+	}
+
+
+	// segundo tipo de validação
+	/*var mensagens = "";
+
+	if(nome == ""){
+		mensagens += "Nome\n"
+	}
+
+	if(cpf == ""){
+		mensagens += "CPF\n"
+	}
+
+	if(mensagens != ""){
+		alert("Por favor preencha os campos abaixo: \n" + mensagens)
+		return;
+	}*/
+
+	var html = "<tr id='linha-" + cpf + "' style='opacity:0.0'>";
 	html += "<td>" + nome + "</td>";
 	html += "<td>" + sobrenome + "</td>";
 	html += "<td>" + cpf + "</td>";
@@ -21,8 +51,61 @@ function cadastrarPessoas(){
 	document.getElementById("telefone").value = "";
 	document.getElementById("endereco").value = "";
 
-	alert("Cadastro concluido com sucesso");
+	//alert("Cadastro concluido com sucesso");
+	//document.getElementById("nome").focus();
 
-	document.getElementById("nome").focus();
+	document.getElementById("formCadastro").style = "display:none";
+	document.getElementById("mostrarForm").style = "display:block";
+
+	// animação com opacity
+	var index = 0;
+	var interval = setInterval(function(){
+		document.getElementById("linha-" + cpf).style = "opacity:0." + index
+		index += 1;
+		if(index > 9){
+			clearInterval(interval);
+		}
+	}, 50)
+
 }
+
+
+function mostrarFormulario(){
+	document.getElementById("mostrarForm").style = "display:none";
+	document.getElementById("formCadastro").style = "display:block";
+}
+
+function alterarFilho(){
+	document.getElementById("registros").childNodes[2].childNodes[0].childNodes[0].innerHTML = "danilo"
+}
+
+function alterarPai(){
+	document.getElementById("registros").parentNode.innerHTML += "teste"
+
+	/*
+	//navegando entre elementos DOM
+	//ir para o proximo
+	document.getElementById("registros").childNodes[2].childNodes[0].childNodes[0].nextSibling
+
+	//ir para o anterior
+	document.getElementById("registros").childNodes[2].childNodes[0].childNodes[1].previousSibling
+	*/
+}
+
+
+/*
+
+// espera e executa somente uma vez
+setTimeout(function(){ 
+	console.log("danilo") 
+}, 2000)
+
+
+//executa até que vc mande parar
+var intervalo = setInterval(function(){ console.log("danilo") }, 2000)
+
+// mandando parar
+clearInterval(intervalo)
+
+*/
 
