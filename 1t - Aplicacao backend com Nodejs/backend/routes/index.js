5c8ec6fa22618918ee96fe5deb00add25f277049
Didox <dansantos@abril.com.br>
2016-03-09 09:15:19 -0300

    gravando em arquivos

--- 1t - Aplicacao backend com Nodejs/backend/routes/index.js
+++ 1t - Aplicacao backend com Nodejs/backend/routes/index.js
@@ -1,25 +1,60 @@
 var express = require('express');
 var router = express.Router();
 var pessoas = [];
+var BANCO_ARQUIVO = "/tmp/bancoArquivo.js";
 
-/* GET home page. */
 router.get('/', function(request, response, next) {
-  response.render('index', { title: 'Node.js com framework express', pessoas: pessoas });
+  dados = { title: 'Node.js com framework express' };
+  carregarBase(function read(err, data) {
+    if (err) { 
+      console.log(err);
+      dados['pessoas'] = [];
+    }
+    else{
+      dados['pessoas'] = JSON.parse(data);
+    }
+
+    response.render('index', dados)
+  });
 });
 
 router.post('/cadastrar-pessoa', function(request, response, next) {
+  carregarBase(function read(err, data) {
+    if (err) { 
+      console.log(err);
+      return;
+    }
 
-  var nome = request.body.nome;
-  hash = {
-    nome: request.body.nome,
-    sobrenome: request.body.sobrenome,
-    cpf: request.body.cpf,
-    telefone: request.body.telefone,
-    endereco: request.body.endereco
-  }
+    pessoas = JSON.parse(data);
 
-  pessoas.push(hash);
-  response.render('index', { title: 'cadastrar-pessoa', pessoas: pessoas });
+    var hash = {
+      nome: request.body.nome,
+      sobrenome: request.body.sobrenome,
+      cpf: request.body.cpf,
+      telefone: request.body.telefone,
+      endereco: request.body.endereco
+    }
+
+    salvarBase(hash)
+    response.render('index', { title: 'cadastrar-pessoa', pessoas: pessoas });
+  });
 });
+
+
+// funcoes auxiliares para arquivos
+var carregarBase = function(callback){
+  var fs = require('fs');
+  fs.readFile(BANCO_ARQUIVO, callback);
+}
+
+var salvarBase = function(hash){
+  pessoas.push(hash);
+  var fs = require('fs');
+  fs.writeFile(BANCO_ARQUIVO, JSON.stringify(pessoas), function(err) {
+    if(err) {
+      console.log(err);
+    }
+  });
+}
 
 module.exports = router;
