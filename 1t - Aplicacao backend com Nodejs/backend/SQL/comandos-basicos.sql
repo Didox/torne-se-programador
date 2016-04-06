f3a697565f23a5776f0c9895cc8d84fd3d64d62f
Didox <dansantos@abril.com.br>
2016-04-06 11:05:30 -0300

    SQL

---
+++ 1t - Aplicacao backend com Nodejs/backend/SQL/comandos-basicos.sql
@@ -0,0 +1,8 @@
+
+Comandos primários do SQL
+
+SELECT * FROM teste.teste2 WHERE `cpf`='535253';
+INSERT INTO `teste`.`teste2` (`cpf`, `nome`, `sobrenome`, `telefone`, `endereco`) VALUES ('535253', 'resre', 'yytra', '65654', 'rua teste');
+UPDATE `teste`.`teste2` SET `sobrenome`='teste', `telefone`='456-663' WHERE `cpf`='552333';
+DELETE FROM `teste`.`teste2` WHERE `cpf`='535253';
+
