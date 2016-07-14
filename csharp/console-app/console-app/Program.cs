56ebf43d577e6bbd03e52870c4c48576cf329dcc
Didox Aparecido <Didox Aparecido>
2016-07-14 13:43:28 -0300

    array c#

--- csharp/console-app/console-app/Program.cs
+++ csharp/console-app/console-app/Program.cs
@@ -10,15 +10,40 @@ namespace console_app
     {
         static void Main(string[] args)
         {
-            looping(1);
-        }
+            string[] a1 = new string[3];
+            a1[0] = "Danilo1";
+            a1[1] = "Danilo2";
+            a1[2] = "Danilo3";
 
-        public static void looping(int a)
-        {
-            Console.WriteLine(a);
-            if(a < 3) {
-                looping(a + 1);
-            } 
+            int size = a1.Length;
+            if(Array.Exists(a1, e=>e.IndexOf("Danilo") != -1))
+            {
+                Console.WriteLine("Existe o Danilo1 no meu array");
+            }
+
+
+            List<string> a = new List<string>();
+            a.Add("Danilo");
+            a.Add("Sheila");
+            a.Add("Lana");
+
+            foreach (string nome in a)
+            {
+                Console.WriteLine(nome);
+            }
+
+            if (a.Exists(e => e == "Danilo"))
+            {
+                Console.WriteLine("Encontrei o danilo no meu collection list");
+            }
+
+            int size_list = a.Count;
+
+
+
+
+
+            Console.Read();
         }
     }
 }
